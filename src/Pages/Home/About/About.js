c99ed5b8e94d93fcb052cf8585a85a7239e6499a
rray524 <rray524@gmail.com>
2021-10-20 21:11:18 +0600

    about section done

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import aboutImg from '../../../images/about-us.png'
+import aboutImg from '../../../images/about-us.png';
+import './About.css'
 
 const About = () => {
     return (
-        <Container>
-            {/* Stack the columns on mobile by making one full-width and the other half-width */}
-            <Row>
-                <Col xs={12} md={6}>
-                    <img src={aboutImg} alt="" />
-                </Col>
-                <Col xs={6} md={6}>
-                    xs=6 md=4
-                </Col>
-            </Row>
-        </Container>
+        <div className="about-wrap">
+            <Container>
+
+                <Row>
+                    <Col xs={12} md={6}>
+                        <div className="about-img">
+                            <img src={aboutImg} alt="" />
+                        </div>
+                    </Col>
+                    <Col xs={12} md={6} className="mt-5">
+                        <h2>About Us</h2>
+                        <p>Ports Mouth Surgery has been servicing the local community since 1985.  Watsonia Road Medical Centre, a new purpose-built clinic opened in 2019 to cater for the growing needs of our patients.  The doctors from both clinics have always endeavoured to provide the highest quality of service with professionalism and compassion.  With the wide range of special areas of interest, expertise and skills among the medical staff at the two clinics, you will find all your healthcare needs will be well managed.  All the staff believe in holistic management of each individual, encompassing mental, physical and emotional health. </p>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+
     );
 };
 

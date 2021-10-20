271057152446048270617c3ffe2a052239379939
rray524 <rray524@gmail.com>
2021-10-21 01:32:03 +0600

    fixed issue

--- src/App.js
+++ src/App.js
@@ -6,31 +6,31 @@ import Fees from './Pages/Fees/Fees';
 import Contact from './Pages/Contact/Contact';
 import AboutUs from './Pages/AboutUs/AboutUs';
 import Footer from './Pages/Shared/Footer/Footer';
-import AuthProvider from './Pages/context/AuthProvider';
+
 
 function App() {
   return (
     <div className="container-wrap">
-      <AuthProvider>
-        <BrowserRouter>
-          <Header></Header>
-          <Switch>
-            <Route exact path="/">
-              <Home></Home>
-            </Route>
-            <Route path="/about">
-              <AboutUs></AboutUs>
-            </Route>
-            <Route path="/fees">
-              <Fees></Fees>
-            </Route>
-            <Route path="/contact">
-              <Contact></Contact>
-            </Route>
-          </Switch>
-          <Footer></Footer>
-        </BrowserRouter>
-      </AuthProvider>
+
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/about">
+            <AboutUs></AboutUs>
+          </Route>
+          <Route path="/fees">
+            <Fees></Fees>
+          </Route>
+          <Route path="/contact">
+            <Contact></Contact>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </BrowserRouter>
+
 
 
     </div>
